83899ecf8d393103e9f920cd6a09eed62b9cd5c7
woodrow-snow <woodrow2021@outlook.com>
2026-03-20 10:06:45 -0600

    uploading work to chapter 13

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,30 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '../../ui/dashboard/revenue-chart';
+import LatestInvoices from '../../ui/dashboard/latest-invoices';
+import { lusitana } from '../../ui/fonts';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import CardsWrapper from '@/app/ui/dashboard/cards';
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2x1`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardsWrapper />
+                </Suspense>
+            </div>
+            <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 gl:grid-cols-8'>
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+            </div>
+        </main>
+    );
+}
